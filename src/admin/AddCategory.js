11ee054bd632129ece73ed45b363ba944c4cf5d6
Your Name <email@example.com>
2021-06-14 21:33:55 +0000

    created add category component

---
+++ src/admin/AddCategory.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import Layout from "../core/Layout";
+import { isAuthenticated } from "../auth";
+import { Link } from "react-router-dom";
+import { createCategory } from "./apiAdmin";
+
+const AddCategory = () => {
+  const [name, setName] = useState("");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  //destructuring user and token from localStorage
+
+  const { user, token } = isAuthenticated();
+
+  const handleChange = (e) => {
+    setError("");
+    setName(e.target.value);
+  };
+  const clickSubmit = (e) => {
+    e.preventDefault();
+    setError("");
+    setSuccess(false);
+    //make request ot api to create category
+    createCategory(user._id, token, { name }).then((data) => {
+      if (data.error) {
+        setError(true);
+      } else {
+        setError("");
+        setSuccess(true);
+      }
+    });
+  };
+  const newCategoryForm = () => (
+    <form onSubmit={clickSubmit}>
+      <div className="form-floating">
+        <label className="text-muted">Name</label>
+        <input
+          type="text"
+          className="form-control"
+          onChange={handleChange}
+          value={name}
+          autoFocus
+          required
+        />
+      </div>
+      <button className="mt-3 btn btn-outline-primary">Create Category</button>
+    </form>
+  );
+
+  const showSuccess = () => {
+    if (success) {
+      return (
+        <h4 className="text-success">{name} has been created successfully</h4>
+      );
+    }
+  };
+
+  const showError = () => {
+    if (error) {
+      return <h4 className="text-danger">Category should be unique</h4>;
+    }
+  };
+
+  const goBack = () => (
+    <div className="mt-5">
+      <Link to="/admin/dashboard" className="text-warning">
+        Back to Dashboard
+      </Link>
+    </div>
+  );
+
+  return (
+    <Layout
+      title="Create Category"
+      description={`Welcome Admin ${user.name}! add a new Category`}
+    >
+      <div className="row">
+        <div className="col-md-8 offset-md-2">
+          {showSuccess()}
+          {showError()}
+          {newCategoryForm()}
+          {goBack()}
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default AddCategory;
