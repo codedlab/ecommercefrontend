104174ce00418ecae92e70ba8e72d162f7e11d0d
Your Name <email@example.com>
2021-06-14 12:00:59 +0000

    Sigin and Signup pages and functions successfully created and tested.

--- src/User/Signin.js
+++ src/User/Signin.js
@@ -1,11 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
 import Layout from "../core/Layout";
+import { signin } from "../auth";
 
-const Signin = () => (
-  <Layout
-    title="Signin"
-    description="Sign into Node React E-commerce App"
-  ></Layout>
-);
+const Signin = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    redirectToReferrer: false,
+  });
+  const { email, password, loading, error, redirectToReferrer } = values;
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
 
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password }).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        setValues({
+          ...values,
+          redirectToReferrer: true,
+        });
+      }
+    });
+  };
+  const signUpForm = () => (
+    <form>
+      <div className="form-floating">
+        <label className="text-muted">Email: </label>
+        <input
+          onChange={handleChange("email")}
+          type="email"
+          className="form-control"
+          value={email}
+        />
+      </div>
+      <div className="form-floating">
+        <label className="text-muted">Password: </label>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          className="form-control"
+          value={password}
+        />
+      </div>
+      <div className="d-grid gap-2 col-6 mx-auto">
+        <button
+          onClick={clickSubmit}
+          className="mt-3 m-5 w-60 btn btn-lg btn-primary"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+  const showLoading = () =>
+    loading && (
+      <div className="alert alert-info">
+        <h2>Loading...</h2>
+      </div>
+    );
+
+  const redirectUser = () => {
+    if (redirectToReferrer) {
+      return <Redirect to="/" />;
+    }
+  };
+  return (
+    <Layout
+      title="Signup"
+      description="Signup into Node React E-commerce App"
+      className="container col-md-8 offset-md-2"
+    >
+      {showError()}
+      {showLoading()}
+      {signUpForm()}
+      {redirectUser()}
+    </Layout>
+  );
+};
 export default Signin;
